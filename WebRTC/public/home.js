2594168d8580d8f0a87690e9c17c780aff097758
Tim Willaert <tim.willaert@student.ehb.be>
2020-09-30 18:13:03 +0200

    Homepage styling

--- WebRTC/public/home.js
+++ WebRTC/public/home.js
@@ -1,6 +1,8 @@
 function joinRoom(){
-    const roomId = prompt('Enter room id')
-    if(roomId != null){
+    const roomId = document.getElementById('input').value;
+    if(roomId != ''){
         window.location.replace('/'+roomId)
+    } else{
+        document.getElementById('input').style.borderBottomColor = 'rgba(245, 37, 37, 0.603)';
     }
 }
