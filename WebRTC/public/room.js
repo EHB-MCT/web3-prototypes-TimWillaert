71b3d04f4535de78b9fe6e070dd6212a0456c90d
Tim Willaert <tim.willaert@student.ehb.be>
2020-10-01 00:46:18 +0200

    Chat & AI

--- WebRTC/public/room.js
+++ WebRTC/public/room.js
@@ -1,4 +1,8 @@
 const socket = io('/')
+
+deepai.setApiKey('73561d32-dcb8-4387-9d6c-e83d0350e158');
+let imgUrl;
+
 const videoGrid = document.getElementById('video-grid')
 
 const roomCode = document.getElementById('room-code')
@@ -11,8 +15,11 @@ myVideo.muted = true
 const peers = {}
 
 let myVideoStream;
+let myUserId;
+
 document.getElementById('unmute').style.display = 'none';
 document.getElementById('showVideo').style.display = 'none';
+document.getElementById('aicontainer').style.display = 'none';
 
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -33,6 +40,28 @@ navigator.mediaDevices.getUserMedia({
         console.log('User connected: ' + userId)
         connectToNewUser(userId, stream)
     })
+
+    let input = document.getElementById('input')
+    input.addEventListener('keyup', function(event){
+        event.preventDefault()
+        if(event.key === 13 || event.key === 'Enter'){
+            if(input.value != ''){
+                socket.emit('message', {value: input.value, userId: myUserId})
+                input.value = ''
+            }
+        }
+    })
+
+    socket.on('createMessage', message => {
+        let chat = document.getElementById('chat')
+        let msg = document.createElement('p')
+        if(message.userId == myUserId){
+            msg.className = 'my-message'
+        }
+        msg.innerHTML = message.value
+        chat.append(msg)
+        scrollToBottom()
+    })
 })
 
 socket.on('user-disconnected', userId => {
@@ -42,6 +71,7 @@ socket.on('user-disconnected', userId => {
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
+    myUserId = id
 })
 
 function addVideoStream(video, stream){
@@ -96,12 +126,42 @@ function toggleVideo(){
     }
 }
 
+function scrollToBottom(){
+    let chat = document.getElementById('chat')
+    chat.scrollTop = chat.scrollHeight
+}
+
+function viewImage(){
+    document.getElementById('aicontainer').style.display = 'block'
+    if(!imgUrl){
+        getImage().then((resp) => {
+            document.getElementById('aiimage').src = resp.output_url
+            imgUrl = resp.output_url
+        })
+    }
+}
+
+async function getImage(){
+    let input = ROOM_ID.replaceAll('-', ' ')
+    var resp = await deepai.callStandardApi("text2img", {
+        text: input,
+    });
+    return resp
+}
+
+function closeImage(){
+    document.getElementById('aicontainer').style.display = 'none';
+}
+
 tippy('#copyCode', {
     content: 'Copied!',
-    trigger: 'click',
-    duration: 200
+    trigger: 'click'
 });
 
 tippy('#copyCode', {
     content: 'Copy to clipboard',
+});
+
+tippy('#viewImage', {
+    content: 'View AI Generated Image',
 });
