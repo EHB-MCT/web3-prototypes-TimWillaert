910c03ed9fe60d5ae441471b004238277959ad1f
Tim Willaert <tim.willaert@student.ehb.be>
2020-10-05 16:51:36 +0200

    React - Delete & Complete Item

--- react-prototype/src/App.js
+++ react-prototype/src/App.js
@@ -17,17 +17,27 @@ class App extends React.Component {
 
   handleEnter = (event) => {
     if(this.state.value != '' && event.key == 'Enter'){
-      const newItem = {value: this.state.value, completed: false}
-      this.setState({todos: this.state.todos.concat(newItem), value: ''})
+      const newItem = this.state.value
+      const newList = this.state.todos
+      newList.push(newItem)
+      this.setState({todos: newList, value: ''})
     }
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
     if(this.state.value != ''){
-      const newItem = {value: this.state.value, completed: false}
-      this.setState({todos: this.state.todos.concat(newItem), value: ''})
+      const newItem = this.state.value
+      const newList = this.state.todos
+      newList.push(newItem)
+      this.setState({todos: newList, value: ''})
     }
+  }
+
+  handleDelete = (index) => {
+    const newList = this.state.todos
+    newList.splice(index, 1)
+    this.setState({todos: newList})
   }
 
   render(){
@@ -38,31 +48,51 @@ class App extends React.Component {
           <input type="text" value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleEnter} placeholder="To do.."></input>
           <button onClick={this.handleSubmit}><FontAwesomeIcon icon={faPlus} /> Add item</button>
         </div>
-          <TodoList list={this.state.todos}></TodoList>
+          <TodoList list={this.state.todos} deleteMethod={this.handleDelete}></TodoList>
       </div>
     )
   }
 }
 
 function TodoList(props){
-  const list = props.list.map((item, index) => <TodoItem key={index} value={item.value} />)
+  const list = props.list.map((item, index) => <TodoItem key={index} index={index} value={item} deleteMethod={props.deleteMethod}/>)
   return(<ul>{list}</ul>)
 }
 
-function TodoItem(props){
-  return(
-    <li>
-      {props.value}
+class TodoItem extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = ({deleted: false, completed: false})
+  }
+
+  handleDelete = () => {
+    this.setState({deleted: true})
+    setTimeout(() => {
+      this.setState({deleted: false})
+      this.props.deleteMethod(this.props.index)
+    }, 500)
+  }
+
+  handleComplete = () => {
+    this.setState({completed: true})
+  }
+
+  render(){
+    return(
+    <li className= {this.state.deleted ? "deleted" : "", this.state.completed ? "completed" : ""}>
+      {this.props.value}
       <div className="operations">
-        <div className="complete" >
+        <div className="complete" onClick={this.handleComplete}>
           <FontAwesomeIcon icon={faCheck} />
         </div>
-        <div className="delete" >
+        <div className="delete" onClick={this.handleDelete}>
           <FontAwesomeIcon icon={faTrash} />
         </div>
       </div>
     </li>
-  )
+    )
+  }
 }
 
 export default App;
