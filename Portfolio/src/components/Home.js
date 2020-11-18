584322cbb05264cc06a553fd2f15431e2ef58dcb
Tim Willaert <tim.willaert@student.ehb.be>
2020-11-18 19:58:43 +0100

    Portfolio - Home

--- Portfolio/src/components/Home.js
+++ Portfolio/src/components/Home.js
@@ -1,43 +1,103 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import AnimatedCursor from "react-animated-cursor";
-import ImageDistort from "react-image-list-distort";
-import ParallaxMousemove from "react-parallax-mousemove";
-import GlitchClip from "react-glitch-effect/core/Clip";
-import FadeIn from "react-fade-in";
 import "./Home.css";
-import image from "../img/construction.jpg";
+import { Link } from "react-router-dom";
+import BIRDS from "vanta/dist/vanta.birds.min";
+import * as THREE from "three";
+import TextTransition, { presets } from "react-text-transition";
+import { useSpring, animated } from "react-spring";
+
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const trans = (x, y) => `translate3d(${-(x / 27)}px,${-(y / 27)}px,0)`;
 
 export default function Home() {
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
+
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const [lineText, setLineText] = useState("Portfolio");
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    const effect = BIRDS({
+      THREE,
+      el: myRef.current,
+      backgroundColor: 0xe8e8e8,
+      color1: 0x8030dc,
+      color2: 0x666666,
+    });
+    if (!vantaEffect) {
+      setVantaEffect(effect);
+      let myInterval = setInterval(() => {
+        window.dispatchEvent(new Event("resize"));
+      }, 1);
+      setTimeout(() => {
+        clearInterval(myInterval);
+      }, 3000);
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="container">
-      <AnimatedCursor color="255, 255, 255" />
-      <div className="overlay"></div>
-      <div className="content">
-        <ul className="myListRoot">
-          <li className="myListItem">
-            <FadeIn transitionDuration="1000" delay="300">
-              <GlitchClip>
-                <p className="link">under construction</p>
-              </GlitchClip>
-            </FadeIn>
-            <img src={image} />
-          </li>
-        </ul>
+    <div
+      id="container"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+    >
+      <AnimatedCursor
+        color="150, 150, 150"
+        outerAlpha="0.2"
+        outerScale="8"
+        innerScale="0.4"
+      />
+      <div id="overlay"></div>
+      <div id="content">
+        <div id="header">
+          <Link to="/work">Work</Link>
+          <div>
+            <p>
+              designer<br></br>
+              <span className="smaller">x</span>
+              <br></br>developer
+            </p>
+          </div>
+          <Link to="/about">About</Link>
+        </div>
+        <div id="main">
+          <div id="effect" ref={myRef}></div>
+          <animated.div style={{ transform: props.xy.interpolate(trans) }}>
+            <Link to="/work">
+              <h1
+                onMouseOver={() => setLineText("Explore")}
+                onMouseLeave={() => setLineText("Portfolio")}
+              >
+                Tim<br></br>Willaert
+              </h1>
+            </Link>
+          </animated.div>
+          <animated.div style={{ transform: props.xy.interpolate(trans) }}>
+            <div id="lineDiv">
+              <span className="line"></span>
+              <h2>
+                <TextTransition
+                  text={lineText}
+                  springConfig={presets.gentle}
+                  direction={lineText === "Portfolio" ? "down" : "up"}
+                />
+              </h2>
+            </div>
+          </animated.div>
+        </div>
+        <div id="footer">
+          <div>
+            <p>Scroll to discover</p>
+            <span className="scroll-line"></span>
+          </div>
+        </div>
       </div>
-      <ImageDistort
-        styles={{ zIndex: 10 }}
-        listRoot={".myListRoot"}
-        itemRoot={".myListItem"}
-        options={{
-          strength: 0.2,
-          effect: "stretch",
-          geometry: {
-            shape: "plane",
-            radius: 0.5,
-            segments: 128,
-          },
-        }}
-      ></ImageDistort>
     </div>
   );
 }
