fe8fe2e8d229e0d313114e1534991a0ec28dc786
Tim Willaert <tim.willaert@student.ehb.be>
2020-11-23 16:47:44 +0100

    Portfolio - About Page

--- Portfolio/src/components/About.js
+++ Portfolio/src/components/About.js
@@ -3,24 +3,53 @@ import "./About.css";
 import AnimatedCursor from "react-animated-cursor";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useSpring, animated } from "react-spring";
 
-export default function About(){
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const trans = (x, y) => `translate3d(${-(x / 27)}px,${-(y / 27)}px,0)`;
 
-    document.title = "Tim Willaert - About"
+export default function About() {
+  document.title = "Tim Willaert - About";
 
-    return(
-        <div id="container-about">
-            <AnimatedCursor
-                color="150, 150, 150"
-                outerAlpha="0.3"
-                outerScale="8"
-                innerScale="0.4"
-            />
-            <div id="overlay"></div>
-            <div id="content">
-                <Header color="dark" />
-                <Footer />
-            </div>
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
+
+  return (
+    <div
+      id="container-about"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+    >
+      <AnimatedCursor
+        color="150, 150, 150"
+        outerAlpha="0.3"
+        outerScale="8"
+        innerScale="0.4"
+      />
+      <p id="about">About</p>
+      <div id="overlay"></div>
+      <div id="content">
+        <Header color="dark" />
+        <div id="main">
+          <animated.div style={{ transform: props.xy.interpolate(trans) }}>
+            <img src="dist/img/tim.png"></img>
+            <p>
+              Hello! I am a designer and full-stack developer from Belgium
+              specializing in creating multimedia applications and experiences.
+            </p>
+          </animated.div>
+          <div id="contact">
+            <a href="https://www.linkedin.com/in/timwillaert/" target="_blank">
+              LinkedIn
+            </a>
+            <a HREF="mailto:tim.willaert@outlook.com">
+              tim.willaert@outlook.com
+            </a>
+          </div>
         </div>
-    )
+        <Footer />
+      </div>
+    </div>
+  );
 }
--- Portfolio/src/components/WorkDetail.js
+++ Portfolio/src/components/WorkDetail.js
@@ -1,4 +1,9 @@
-import React, { useLayoutEffect, useEffect, useState, forceUpdate } from "react";
+import React, {
+  useLayoutEffect,
+  useEffect,
+  useState,
+  forceUpdate,
+} from "react";
 import { useParams } from "react-router-dom";
 import AnimatedCursor from "react-animated-cursor";
 import "./WorkDetail.css";
@@ -12,165 +17,183 @@ import ScrollToTop from "react-scroll-up";
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
 const trans = (x, y) => `translate3d(${-(x / 27)}px,${-(y / 27)}px,0)`;
 
-export default function WorkDetail(){
-    const { id } = useParams();
-    const [work, setWork] = useState({});
-    const [workNumber, setWorkNumber] = useState(0);
+export default function WorkDetail() {
+  const { id } = useParams();
+  const [work, setWork] = useState({});
+  const [workNumber, setWorkNumber] = useState(0);
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-        let obj = Works.find(obj => obj.pathName === id);
-        setWorkNumber(Works.indexOf(obj));
-        setWork(obj);
-        document.title = "Tim Willaert - " + obj.title;
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    let obj = Works.find((obj) => obj.pathName === id);
+    setWorkNumber(Works.indexOf(obj));
+    setWork(obj);
+    document.title = "Tim Willaert - " + obj.title;
+  }, []);
 
-    const [props, set] = useSpring(() => ({
-        xy: [0, 0],
-        config: { mass: 10, tension: 550, friction: 140 },
-      }));
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
 
-    return(
-        <div id="container-detail" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-            <AnimatedCursor
-                color="150, 150, 150"
-                outerAlpha="0.2"
-                outerScale="8"
-                innerScale="0.4"
-            />
-            <div id="overlay"></div>
-            <div id="content-detail">
-                <ProgressBar bgcolor="#7B08FF"/>
-                <ScrollToTop showUnder={250} style={{right: 50}}>
-                    <span className="up link">Top</span>
-                </ScrollToTop>
-                <div id="header-detail">
-                    <NavLink to="/work">
-                        Close
-                    </NavLink>
-                </div>
-                <div id="main-detail">
-                    <div id="worklanding">
-                        <div id="metadata">
-                            <h3>{"Project 0" + (workNumber + 1)}</h3>
-                            <h3>{work.year}</h3>
-                        </div>
-                        {
-                            work.thumbnail != undefined &&
-                            <div>
-                                <animated.div className="parent" style={{ transform: props.xy.interpolate(trans) }}>
-                                    <div className="child">
-                                        <h1>{work.title}</h1>
-                                        <img src={`dist/img/${work.thumbnail}`}></img>
-                                    </div>
-                                </animated.div>
-                                <ImageDistort
-                                    styles={{ zIndex: 10 }}
-                                    listRoot={".parent"}
-                                    itemRoot={".child"}
-                                    options={{
-                                    strength: 0.2,
-                                    geometry: {
-                                        shape: "plane",
-                                        width: 0.8,
-                                        height: 0.8,
-                                    },
-                                    }}
-                                ></ImageDistort>
-                            </div>
-                        }
-                        <div id="details">
-                            <p>{work.description}</p>
-                            <a href={work.url} target="_blank">Visit Website</a>
-                        </div>
-                    </div>
-                    <div id="scroll-detail">
-                        <div>
-                            <p>Scroll to see more</p>
-                            <span className="scroll-line"></span>
-                        </div>
-                    </div>
-                    <div id="work-explanation">
-                    {work.explanation !== undefined &&
-                        work.explanation.map((data, key) => {
-                            return(
-                                    <div className="workExpl" key={key}>
-                                        <div>
-                                            <p>{"0" + (key + 1)}</p>
-                                            <p>{data.text}</p>
-                                        </div>
-                                        {
-                                            data.img &&
-                                            <img src={`dist/img/${data.img}`}></img>
-                                        }
-                                        {
-                                            data.video &&
-                                            <video controls src={`dist/img/${data.video}`}></video>
-                                        }
-                                    </div>
-                            )
-                        })
-                    }
-                    </div>
-                    <div id="links">
-                        <a href={work.url} target="_blank">Visit Website</a>
-                        <a href={work.github} target="_blank">View Code</a>
-                    </div>
-                    <div id="nextProject">
-                        <p>Next project</p>
-                            {
-                                (work.thumbnail !== undefined && workNumber + 2 <= Works.length) &&
-                                <div className="link">
-                                    <animated.div className="parent2" style={{ transform: props.xy.interpolate(trans) }}>
-                                        <NavLink to={"/work/"+Works[workNumber + 1].pathName} className="child2">
-                                            <h5>{"0" + (workNumber + 2)}</h5>
-                                            <h4>{Works[workNumber + 1].title}</h4>
-                                            <img src={`dist/img/${Works[workNumber + 1].thumbnail}`}></img>
-                                        </NavLink>
-                                    </animated.div>
-                                    <ImageDistort
-                                        styles={{ zIndex: 10 }}
-                                        listRoot={".parent2"}
-                                        itemRoot={".child2"}
-                                        options={{
-                                        strength: 0.2,
-                                        geometry: {
-                                            shape: "plane",
-                                            width: 0.8,
-                                            height: 0.8,
-                                        },
-                                        }}
-                                    ></ImageDistort>
-                                </div>
-                            }
-                            {
-                                workNumber + 2 > Works.length &&
-                                <div className="link">
-                                    <animated.div className="parent2" style={{ transform: props.xy.interpolate(trans) }}>
-                                        <NavLink to={"/work/"+Works[0].pathName} className="child2">
-                                            <h5>01</h5>
-                                            <h4>{Works[0].title}</h4>
-                                            <img src={`dist/img/${Works[0].thumbnail}`}></img>
-                                        </NavLink>
-                                    </animated.div>
-                                    <ImageDistort
-                                        styles={{ zIndex: 10 }}
-                                        listRoot={".parent2"}
-                                        itemRoot={".child2"}
-                                        options={{
-                                        strength: 0.2,
-                                        geometry: {
-                                            shape: "plane",
-                                            width: 0.8,
-                                            height: 0.8,
-                                        },
-                                        }}
-                                    ></ImageDistort>
-                                </div>
-                            }
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      id="container-detail"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+    >
+      <AnimatedCursor
+        color="150, 150, 150"
+        outerAlpha="0.2"
+        outerScale="8"
+        innerScale="0.4"
+      />
+      <div id="overlay"></div>
+      <div id="content-detail">
+        <ProgressBar bgcolor="#7B08FF" />
+        <ScrollToTop showUnder={250} style={{ right: 50 }}>
+          <span className="up link">Top</span>
+        </ScrollToTop>
+        <div id="header-detail">
+          <NavLink to="/work">Close</NavLink>
         </div>
-    )
+        <div id="main-detail">
+          <div id="worklanding">
+            <div id="metadata">
+              <h3>{"Project 0" + (workNumber + 1)}</h3>
+              <h3>{work.year}</h3>
+            </div>
+            {work.thumbnail != undefined && (
+              <div>
+                <animated.div
+                  className="parent"
+                  style={{ transform: props.xy.interpolate(trans) }}
+                >
+                  <div className="child">
+                    <h1>{work.title}</h1>
+                    <img src={`dist/img/${work.thumbnail}`}></img>
+                  </div>
+                </animated.div>
+                <ImageDistort
+                  styles={{ zIndex: 10 }}
+                  listRoot={".parent"}
+                  itemRoot={".child"}
+                  options={{
+                    strength: 0.2,
+                    geometry: {
+                      shape: "plane",
+                      width: 0.8,
+                      height: 0.8,
+                    },
+                  }}
+                ></ImageDistort>
+              </div>
+            )}
+            <div id="details">
+              <p>{work.description}</p>
+              <a href={work.url} target="_blank">
+                Visit Website
+              </a>
+            </div>
+          </div>
+          <div id="scroll-detail">
+            <div>
+              <p>Scroll to see more</p>
+              <span className="scroll-line"></span>
+            </div>
+          </div>
+          <div id="work-explanation">
+            {work.explanation !== undefined &&
+              work.explanation.map((data, key) => {
+                return (
+                  <div className="workExpl" key={key}>
+                    <div>
+                      <p>{"0" + (key + 1)}</p>
+                      <p>{data.text}</p>
+                    </div>
+                    {data.img && <img src={`dist/img/${data.img}`}></img>}
+                    {data.video && (
+                      <video controls src={`dist/img/${data.video}`}></video>
+                    )}
+                  </div>
+                );
+              })}
+          </div>
+          <div id="end-container">
+            <div id="links">
+              <a href={work.url} target="_blank">
+                Visit Website
+              </a>
+              <a href={work.github} target="_blank">
+                View Code
+              </a>
+            </div>
+            <div id="nextProject">
+              <p>Next project</p>
+              {work.thumbnail !== undefined && workNumber + 2 <= Works.length && (
+                <div className="link">
+                  <animated.div
+                    className="parent2"
+                    style={{ transform: props.xy.interpolate(trans) }}
+                  >
+                    <NavLink
+                      to={"/work/" + Works[workNumber + 1].pathName}
+                      className="child2"
+                    >
+                      <h5>{"0" + (workNumber + 2)}</h5>
+                      <h4>{Works[workNumber + 1].title}</h4>
+                      <img
+                        src={`dist/img/${Works[workNumber + 1].thumbnail}`}
+                      ></img>
+                    </NavLink>
+                  </animated.div>
+                  <ImageDistort
+                    styles={{ zIndex: 10 }}
+                    listRoot={".parent2"}
+                    itemRoot={".child2"}
+                    options={{
+                      strength: 0.2,
+                      geometry: {
+                        shape: "plane",
+                        width: 0.8,
+                        height: 0.8,
+                      },
+                    }}
+                  ></ImageDistort>
+                </div>
+              )}
+              {workNumber + 2 > Works.length && (
+                <div className="link">
+                  <animated.div
+                    className="parent2"
+                    style={{ transform: props.xy.interpolate(trans) }}
+                  >
+                    <NavLink
+                      to={"/work/" + Works[0].pathName}
+                      className="child2"
+                    >
+                      <h5>01</h5>
+                      <h4>{Works[0].title}</h4>
+                      <img src={`dist/img/${Works[0].thumbnail}`}></img>
+                    </NavLink>
+                  </animated.div>
+                  <ImageDistort
+                    styles={{ zIndex: 10 }}
+                    listRoot={".parent2"}
+                    itemRoot={".child2"}
+                    options={{
+                      strength: 0.2,
+                      geometry: {
+                        shape: "plane",
+                        width: 0.8,
+                        height: 0.8,
+                      },
+                    }}
+                  ></ImageDistort>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
